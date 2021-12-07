ebbc32c094c2d4f34efe42f8acf6f98db3134c9b
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-12-07 16:36:10 +0100

    migrating to full camembert implementation

--- LOTClass/model.py
+++ LOTClass/model.py
@@ -1,6 +1,10 @@
-from transformers import BertPreTrainedModel, BertModel
-#from transformers import BertPreTrainedModel, CamembertModel
-from transformers.models.bert.modeling_bert import BertOnlyMLMHead
+from transformers import BertPreTrainedModel
+# =================================================================================
+#from transformers import BertModel
+#from transformers.models.bert.modeling_bert import BertOnlyMLMHead
+from transformers import CamembertModel
+from transformers.models.camembert.modeling_camembert import CamembertForMaskedLM
+# =================================================================================
 from torch import nn
 import sys
 
@@ -10,9 +14,10 @@ class LOTClassModel(BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
-        self.bert = BertModel(config, add_pooling_layer=False)
-        #self.bert = CamembertModel(config, add_pooling_layer=False)
-        self.cls = BertOnlyMLMHead(config)
+        #self.bert = BertModel(config, add_pooling_layer=False)
+        self.bert = CamembertModel(config, add_pooling_layer=False)
+        #self.cls = BertOnlyMLMHead(config)
+        self.cls = CamembertForMaskedLM(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.activation = nn.Tanh()
