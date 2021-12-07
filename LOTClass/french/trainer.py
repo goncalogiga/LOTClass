048a65fbca81297d3da833c00d09738e79093db1
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-12-07 18:30:17 +0100

    Almost done

--- LOTClass/french/trainer.py
+++ LOTClass/french/trainer.py
@@ -277,6 +277,7 @@ class LOTClassTrainer(object):
                     input_mask = batch[1].to(rank)
                     label_pos = batch[2].to(rank)
                     match_idx = label_pos >= 0
+                    print(input_ids)
                     predictions = model(input_ids,
                                         pred_mode="mlm",
                                         token_type_ids=None, 
