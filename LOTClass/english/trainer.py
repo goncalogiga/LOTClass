117d2607c18f6acbae9e8dc2c7a673841fd273f8
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-12-07 18:49:00 +0100

    Almost done

--- LOTClass/english/trainer.py
+++ LOTClass/english/trainer.py
@@ -224,7 +224,7 @@ class LOTClassTrainer(object):
 
     # create dataset loader
     def make_dataloader(self, rank, data_dict, batch_size):
-        print(f"data_dict['inputs_ids']: {data_dict['inputs_ids']}")
+        print(f"data_dict['input_ids']: {data_dict['input_ids']}")
         if "labels" in data_dict:
             dataset = TensorDataset(data_dict["input_ids"], data_dict["attention_masks"], data_dict["labels"])
         else:
