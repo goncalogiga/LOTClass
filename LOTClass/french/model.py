997c6ae4ec244629b618327f2b60f778dd4720b1
CASIMIRO-MATIAS-MAIA-GIGA GONCALO <goncalo.casimiro-matias-maia-giga@etu.unistra.fr>
2021-12-08 13:36:57 +0100

    Trying to understand camembert predictions

--- LOTClass/french/model.py
+++ LOTClass/french/model.py
@@ -10,50 +10,51 @@ from torch import nn, zeros
 import sys
 
 
-class CamembertPredictionHeadTransform(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        if isinstance(config.hidden_act, str):
-            self.transform_act_fn = ACT2FN[config.hidden_act]
-        else:
-            self.transform_act_fn = config.hidden_act
-        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+# class CamembertPredictionHeadTransform(nn.Module):
+#     def __init__(self, config):
+#         super().__init__()
+#         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+#         if isinstance(config.hidden_act, str):
+#             self.transform_act_fn = ACT2FN[config.hidden_act]
+#         else:
+#             self.transform_act_fn = config.hidden_act
+#         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
 
-    def forward(self, hidden_states):
-        hidden_states = self.dense(hidden_states)
-        hidden_states = self.transform_act_fn(hidden_states)
-        hidden_states = self.LayerNorm(hidden_states)
-        return hidden_states
+#     def forward(self, hidden_states):
+#         hidden_states = self.dense(hidden_states)
+#         hidden_states = self.transform_act_fn(hidden_states)
+#         hidden_states = self.LayerNorm(hidden_states)
+#         return hidden_states
 
 
-class CamembertLMPredictionHead(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.transform = CamembertPredictionHeadTransform(config)
+# class CamembertLMPredictionHead(nn.Module):
+#     def __init__(self, config):
+#         super().__init__()
+#         self.transform = CamembertPredictionHeadTransform(config)
 
-        # The output weights are the same as the input embeddings, but there is
-        # an output-only bias for each token.
-        self.decoder = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+#         # The output weights are the same as the input embeddings, but there is
+#         # an output-only bias for each token.
+#         self.decoder = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
 
-        self.bias = nn.Parameter(zeros(config.vocab_size))
+#         self.bias = nn.Parameter(zeros(config.vocab_size))
 
-        # Need a link between the two variables so that the bias is correctly resized with `resize_token_embeddings`
-        self.decoder.bias = self.bias
+#         # Need a link between the two variables so that the bias is correctly resized with `resize_token_embeddings`
+#         self.decoder.bias = self.bias
 
-    def forward(self, hidden_states):
-        hidden_states = self.transform(hidden_states)
-        hidden_states = self.decoder(hidden_states)
-        return hidden_states
+#     def forward(self, hidden_states):
+#         hidden_states = self.transform(hidden_states)
+#         hidden_states = self.decoder(hidden_states)
+#         return hidden_states
 
 
 class CamembertOnlyMLMHead(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.predictions = CamembertLMPredictionHead(config)
+        self.model = CamembertForMaskedLM(config)
+        self.model.eval()
 
-    def forward(self, sequence_output):
-        prediction_scores = self.predictions(sequence_output)
+    def forward(self, input_ids):
+        prediction_scores = self.model(input_ids)[0]
         return prediction_scores
 
 
